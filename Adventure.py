a0d1e61f831dc389dfde1964225593789df2cb7b
Jacob Pfeifer <jakepf00@gmail.com>
2014-10-15 22:00:45 -0500

    asked user for name

--- Adventure.py
+++ Adventure.py
@@ -1,8 +1,27 @@
-print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print ("Welcome to the Mountain of Mysteriousness")
-print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
-print("""hi man what do ya do
-1...eat
-2...fight
-3...kill""")
+def title():
+	print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	print ("Welcome to the Mountain of Mysteriousness")
+	print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	print ("~~~By~J~Cornelius~Fifelolz~~~~~~~~~~~~~~~")
+title()
+
+
+print
+name = raw_input("What is your name? ")
+if name == "Jacob": 
+	print ("Jacob you're awesome")
+elif name == "Jake":
+	print ("Jake, thats a pretty cool name.")
+else: 
+	print (name + ", what a crappy name")
+
+
+type = raw_input("Are you a Knight, 1, Wizard, 2, or Troll, 3?")
+if type == "1":
+
+elif type == "2":
+
+elif type == "3":
+
+else:
+	print ("Idiot. Pick again.")
