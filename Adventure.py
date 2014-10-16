2360d150766e9d39aec49f2c197961d6e90efedd
Jacob Pfeifer <jakepf00@gmail.com>
2014-10-16 17:01:30 -0500

    added character types that player can choose

--- Adventure.py
+++ Adventure.py
@@ -16,5 +16,14 @@ else:
 	print (name + ", what a crappy name")
 
 print
-print ("anyway...")
-
+print ("Anyway...")
+type = raw_input("Are you a knight, wizard, or a troll? ")
+if type == "knight":
+	a, d, h = 6, 4, 50
+elif type == "wizard":
+	a, d, h = 8, 1, 40
+elif type == "troll":
+	a, d, h = 5, 4, 45
+else:
+	print ("Wow. Just wow.")
+print ("sir " + name + " the " + type + ", are you ready for adventure?")
